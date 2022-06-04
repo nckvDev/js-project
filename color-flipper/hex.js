236ef9a56c56731b1ab992ac90d6532536569e76
nack <jirapong.k@touchtechnologies.co.th>
2022-06-04 22:11:30 +0700

    style: color flip and reviews

--- color-flipper/hex.js
+++ color-flipper/hex.js
@@ -2,6 +2,7 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
 // #a3f32e
 const btn = document.getElementById('btn')
 const color = document.querySelector('.color')
+const colorLogo = document.querySelector('.color-logo')
 
 btn.addEventListener('click', function () {
   let hexColor = '#'
@@ -9,10 +10,10 @@ btn.addEventListener('click', function () {
   for (let i = 0; i < 6; i++) {
     hexColor += hex[getRandomNumber()]
   }
-  console.log('hexColor', hexColor)
   color.textContent = hexColor
   document.body.style.backgroundColor = hexColor
   color.style.color = hexColor
+  colorLogo.style.color = hexColor
 })
 
 function getRandomNumber() {
